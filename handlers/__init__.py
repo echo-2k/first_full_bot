ba4ca6045c0a5c8e7edd961108019954b6885744
echo-2k <echo2k@internet.ru>
2024-07-26 13:27:59 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,10 +2,12 @@ from .start_handler import register_handlers_start
 from .user_handler import register_handlers_user
 from .file_handler import register_handlers_file
 from .shop_handler import register_handlers_shop
+from .admin_handler import register_handlers_admin
 
 __all__ = [
     "register_handlers_start",
     "register_handlers_user",
     "register_handlers_file",
     "register_handlers_shop",
+    "register_handlers_admin",
 ]
