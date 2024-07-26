efa743807fa27e03839162cb4a084723776a93cc
echo-2k <echo2k@internet.ru>
2024-07-26 12:14:45 +0500

    Delete init_db.py

--- init_db.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from models.product import Product
-
-async def init_db():
-    await Product.add_product("Товар 1", "Описание товара 1", 10.0)
-    await Product.add_product("Товар 2", "Описание товара 2", 15.0)
-
-if __name__ == "__main__":
-    asyncio.run(init_db())
