6df989f5c4a2326a31cdd2d4ae6eed91fe146fac
echo-2k <echo2k@internet.ru>
2024-07-26 13:31:47 +0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -2,17 +2,18 @@ from models.database import db
 
 class User:
     @staticmethod
-    async def add_user(user_id: int, username: str, drive_folder_id: str, service_account_file: str):
+    async def add_admin(user_id: int):
         async with db.pool.acquire() as connection:
             await connection.execute(
-                "INSERT INTO users (user_id, username, drive_folder_id, service_account_file) VALUES ($1, $2, $3, $4)",
-                user_id, username, drive_folder_id, service_account_file
+                "INSERT INTO admins (user_id) VALUES ($1) ON CONFLICT DO NOTHING",
+                user_id
             )
 
     @staticmethod
-    async def get_user(user_id: int):
+    async def is_admin(user_id: int):
         async with db.pool.acquire() as connection:
-            return await connection.fetchrow(
-                "SELECT * FROM users WHERE user_id = $1",
+            result = await connection.fetchval(
+                "SELECT 1 FROM admins WHERE user_id = $1",
                 user_id
             )
+            return bool(result)
