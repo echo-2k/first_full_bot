7e1ce2638742914fefda0407d35043684742d9bd
echo-2k <echo2k@internet.ru>
2024-07-26 13:32:53 +0500

    Update order.py

--- models/order.py
+++ models/order.py
@@ -2,17 +2,14 @@ from models.database import db
 
 class Order:
     @staticmethod
-    async def add_order(user_id: int, product_id: int, payment_method: str):
+    async def add_order(user_id: int, product_id: int, currency: str):
         async with db.pool.acquire() as connection:
             await connection.execute(
-                "INSERT INTO orders (user_id, product_id, payment_method) VALUES ($1, $2, $3)",
-                user_id, product_id, payment_method
+                "INSERT INTO orders (user_id, product_id, currency) VALUES ($1, $2, $3)",
+                user_id, product_id, currency
             )
 
     @staticmethod
     async def get_order(order_id: int):
         async with db.pool.acquire() as connection:
-            return await connection.fetchrow(
-                "SELECT * FROM orders WHERE id = $1",
-                order_id
-            )
+            return await connection.fetchrow("SELECT * FROM orders WHERE id = $1", order_id)
